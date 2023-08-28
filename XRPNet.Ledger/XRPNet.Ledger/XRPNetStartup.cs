22ab23a49d7247dc701dfa5e9e4e5757e875033d
Dan Gerchcovich <dan.developer789@gmail.com>
2023-08-29 06:47:50 +1000

    Initial Commit

---
+++ XRPNet.Ledger/XRPNet.Ledger/XRPNetStartup.cs
@@ -0,0 +1,17 @@
+﻿using Grace.DependencyInjection;
+
+namespace XRPNet.Ledger
+{
+    public class XRPNetStartup
+    {
+        private static readonly DependencyInjectionContainer Container = new DependencyInjectionContainer();
+        public static void InitializeKernel(int httpClientTimeoutSeconds = 120)
+        {
+        }
+
+        public static T GetService<T>()
+        {
+            return Container.Locate<T>();
+        }
+    }
+}
