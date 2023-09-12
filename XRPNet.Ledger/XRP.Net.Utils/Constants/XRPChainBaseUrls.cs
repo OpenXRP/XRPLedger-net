632c7e35c21056d921b80559de2194f74a16fedf
Dan Gerchcovich <dan.developer789@gmail.com>
2023-09-12 21:35:37 +1000

    V1.0.1

---
+++ XRPNet.Ledger/XRP.Net.Utils/Constants/XRPChainBaseUrls.cs
@@ -0,0 +1,12 @@
+﻿namespace XRP.Net.Utils.Constants
+{
+    public class XRPChainBaseUrls
+    {
+        public const string TESTNET = "faucet.altnet.rippletest.net";
+        public const string DEVNET = "faucet.devnet.rippletest.net";
+        public const string AMMDEVNET = "ammfaucet.devnet.rippletest.net";
+        public const string HOOKSV3_TESTNET = "hooks-testnet-v3.xrpl-labs.com";
+        public const string SIDECHAIN_DEVNET = "sidechain-faucet.devnet.rippletest.net";
+        public const string MAIN_NET = "";
+    }
+}
